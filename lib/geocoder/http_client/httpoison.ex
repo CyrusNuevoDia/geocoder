52d3fcc992496218ef8ee93557527d8c99a46848
epinault <kind.moon0724@fastmail.com>
2025-07-18 15:53:52 -0700

    Fix HTTPoison client to handle non-200 status codes (#158)

--- lib/geocoder/http_client/httpoison.ex
+++ lib/geocoder/http_client/httpoison.ex
@@ -9,13 +9,17 @@ defmodule Geocoder.HttpClient.Httpoison do
     def request(%{method: method, url: url, query_params: params} = request, config \\ []) do
       http_client_opts = config[:http_client_opts]
       json_codec = config[:json_codec]
+      httpoison_module = config[:httpoison_module] || HTTPoison
 
       opts = [:with_body | http_client_opts] ++ [ibrowse: [headers_as_is: true], params: params]
       headers = request[:headers] || []
 
-      case HTTPoison.request(method, url, "", headers, opts) do
+      case httpoison_module.request(method, url, "", headers, opts) do
         {:ok, %HTTPoison.Response{status_code: 200, body: body, headers: headers}} ->
           {:ok, %{status_code: 200, body: json_codec.decode!(body), headers: headers}}
+
+        {:ok, %HTTPoison.Response{status_code: code, body: body, headers: headers}} ->
+          {:ok, %{status_code: code, body: body, headers: headers}}
 
         {:error, %HTTPoison.Error{} = error} ->
           {:error, error}
